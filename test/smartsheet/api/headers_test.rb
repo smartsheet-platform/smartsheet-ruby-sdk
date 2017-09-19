ac11d2d290f89d6b652d7034833c5b4ac901d972
Cameron Bowie <cameron.bowie@parivedasolutions.com>
2017-09-19 10:13:21 -0700

    Change tests to match code styling

--- test/smartsheet/api/headers_test.rb
+++ test/smartsheet/api/headers_test.rb
@@ -5,12 +5,12 @@ require_relative '../../../lib/smartsheet/api/headers'
 describe Smartsheet::API::Headers do
   describe Smartsheet::API::Headers::HeaderBuilder do
     before do
-      @headerBuilder = Smartsheet::API::Headers::HeaderBuilder.new("0123456789")
+      @header_builder = Smartsheet::API::Headers::HeaderBuilder.new('0123456789')
       @request = OpenStruct.new
     end
 
-    it "applies defaults" do
-      @headerBuilder.apply(@request)
+    it 'applies defaults' do
+      @header_builder.apply(@request)
 
       @request.headers.wont_be_nil
       @request.headers.must_be_kind_of Hash
@@ -19,18 +19,18 @@ describe Smartsheet::API::Headers do
       @request.headers[:'User-Agent'].must_equal 'smartsheet-ruby-sdk'
     end
 
-    it "applies sending json" do
-      @headerBuilder.sending_json
-      @headerBuilder.apply(@request)
+    it 'applies sending json' do
+      @header_builder.sending_json
+      @header_builder.apply(@request)
 
       @request.headers.wont_be_nil
       @request.headers.must_be_kind_of Hash
       @request.headers[:'Content-Type'].must_equal 'application/json'
     end
 
-    it "applies overrides" do
-      @headerBuilder.with_overrides({SomeOverride: 'someValue', Authorization: 'someAuth'})
-      @headerBuilder.apply(@request)
+    it 'applies overrides' do
+      @header_builder.with_overrides({SomeOverride: 'someValue', Authorization: 'someAuth'})
+      @header_builder.apply(@request)
 
       @request.headers.wont_be_nil
       @request.headers.must_be_kind_of Hash
@@ -40,15 +40,15 @@ describe Smartsheet::API::Headers do
     end
   end
 
-  describe "#build_headers" do
+  describe '#build_headers' do
     before do
       @object = OpenStruct.new
-      @object.token = "0123456789"
+      @object.token = '0123456789'
       @object.extend(Smartsheet::API::Headers)
       @request = OpenStruct.new
     end
 
-    it "uses token" do
+    it 'uses token' do
       headers = @object.build_headers
       headers.apply(@request)
 
@@ -57,7 +57,7 @@ describe Smartsheet::API::Headers do
       @request.headers[:Authorization].must_equal 'Bearer 0123456789'
     end
 
-    it "uses overrides" do
+    it 'uses overrides' do
       headers = @object.build_headers({SomeOverride: 'someValue'})
       headers.apply(@request)
 
--- test/smartsheet/api/urls_test.rb
+++ test/smartsheet/api/urls_test.rb
@@ -8,22 +8,22 @@ module Smartsheet
       @object.extend(Smartsheet::API::URLs)
     end
 
-    describe "#build_urls" do
-      it "returns slash separated url" do
-        @object.build_url(*["a", "b", "c"]).must_equal "https://api.smartsheet.com/2.0/a/b/c"
+    describe '#build_urls' do
+      it 'returns slash separated url' do
+        @object.build_url(*%w[a b c]).must_equal 'https://api.smartsheet.com/2.0/a/b/c'
       end
 
-      it "has correct base" do
-        @object.build_url(*[]).must_equal "https://api.smartsheet.com/2.0"
+      it 'has correct base' do
+        @object.build_url(*[]).must_equal 'https://api.smartsheet.com/2.0'
       end
 
-      it "handles symbols" do
-        @object.build_url(*["a", :a_id, "b", :b_id], context: {a_id: 123, b_id: 234}).must_equal "https://api.smartsheet.com/2.0/a/123/b/234"
+      it 'handles symbols' do
+        @object.build_url(*['a', :a_id, 'b', :b_id], context: {a_id: 123, b_id: 234}).must_equal 'https://api.smartsheet.com/2.0/a/123/b/234'
       end
 
-      it "raises on missing symbol" do
+      it 'raises on missing symbol' do
         # TODO: This isn't great behavior. We should probably throw some sort of exception when the context is incomplete
-        @object.build_url(*["a", :a_id, "b", :b_id], context: {a_id: 123}).must_equal "https://api.smartsheet.com/2.0/a/123/b/"
+        @object.build_url(*['a', :a_id, 'b', :b_id], context: {a_id: 123}).must_equal 'https://api.smartsheet.com/2.0/a/123/b/'
       end
     end
   end
