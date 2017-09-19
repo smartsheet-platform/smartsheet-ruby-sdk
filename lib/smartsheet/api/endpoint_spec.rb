5395801eadfdf3de50219b2df2b7a2a789e73b92
Nathan Armstrong <kenogu.labz@gmail.com>
2017-09-19 10:59:42 -0700

    Tidy up endpoint spec.

--- lib/smartsheet/api/endpoint_spec.rb
+++ lib/smartsheet/api/endpoint_spec.rb
@@ -5,12 +5,16 @@ module Smartsheet
     class EndpointSpec
       REQUIRED_KEYS = %i[symbol method url].freeze
       OPTIONAL_KEYS = %i[has_params headers body_type].freeze
+      VALID_KEYS = (REQUIRED_KEYS + OPTIONAL_KEYS).freeze
 
       attr_reader :symbol, :method
 
       def initialize(spec)
-        @symbol, @method, @url = REQUIRED_KEYS.collect { |k| spec[k] }
+        @symbol = spec[:symbol]
+        @method = spec[:method]
+        @url = spec[:url]
         @spec = spec
+
         validate
       end
 
@@ -28,7 +32,7 @@ module Smartsheet
       end
 
       def sending_json?
-        accepts_body? && spec[:body_type] == :json
+        requires_body? && spec[:body_type] == :json
       end
 
       def url_segments
@@ -44,11 +48,10 @@ module Smartsheet
       attr_reader :url, :spec
 
       def validate_keys
-        missing_keys = REQUIRED_KEYS.reject { |key| spec.key? key }
+        missing_keys = REQUIRED_KEYS - spec.keys
         raise "Missing required endpoint spec keys: #{missing_keys}" unless missing_keys.empty?
 
-        valid_keys = REQUIRED_KEYS + OPTIONAL_KEYS
-        invalid_keys = spec.keys.reject { |k| valid_keys.include? k }
+        invalid_keys = spec.keys - VALID_KEYS
         raise "Invalid endpoint spec keys: #{invalid_keys}" unless invalid_keys.empty?
       end
 
@@ -57,10 +60,6 @@ module Smartsheet
         valid_methods = %i[get put post delete]
         is_method_valid = valid_methods.any? { |m| m == method }
         raise "Invalid endpoint method: #{method}" unless is_method_valid
-      end
-
-      def accepts_body?
-        spec.key? :body_type
       end
     end
   end
