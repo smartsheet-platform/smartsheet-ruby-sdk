d530e2f91203bf209ef61c7702a876ab4ea5b4b9
Cameron Bowie <cameron.bowie@parivedasolutions.com>
2017-09-19 14:48:04 -0700

    Update URL tests

--- test/smartsheet/api/urls_test.rb
+++ test/smartsheet/api/urls_test.rb
@@ -1,30 +1,40 @@
 require 'minitest/autorun'
 require_relative '../../../lib/smartsheet/api/urls'
+require_relative '../../../lib/smartsheet/api/endpoint_spec'
+require_relative '../../../lib/smartsheet/api/request_spec'
 
-module Smartsheet
-  describe Smartsheet::API::URLs do
-    before do
-      @object = Object.new
-      @object.extend(Smartsheet::API::URLs)
+describe Smartsheet::API::UrlBuilder do
+  before do
+    @url_builder = Smartsheet::API::UrlBuilder.new
+  end
+
+  describe '#build_urls' do
+    it 'returns slash separated url' do
+      @url_builder
+          .for_endpoint(Smartsheet::API::EndpointSpec.new({symbol: :asdf, method: :get, url: %w[a b c]}))
+          .for_request(Smartsheet::API::RequestSpec.new(path_args: {}, params:{}, header_overrides: {}, body:{}))
+      @url_builder.build().must_equal 'https://api.smartsheet.com/2.0/a/b/c'
     end
 
-    describe '#build_urls' do
-      it 'returns slash separated url' do
-        @object.build_url(*%w[a b c]).must_equal 'https://api.smartsheet.com/2.0/a/b/c'
-      end
+    it 'has correct base' do
+      @url_builder
+          .for_endpoint(Smartsheet::API::EndpointSpec.new({symbol: :asdf, method: :get, url: []}))
+          .for_request(Smartsheet::API::RequestSpec.new(path_args: {}, params:{}, header_overrides: {}, body:{}))
+      @url_builder.build().must_equal 'https://api.smartsheet.com/2.0'
+    end
 
-      it 'has correct base' do
-        @object.build_url(*[]).must_equal 'https://api.smartsheet.com/2.0'
-      end
+    it 'handles symbols' do
+      @url_builder
+          .for_endpoint(Smartsheet::API::EndpointSpec.new({symbol: :asdf, method: :get, url: ['a', :a_id, 'b', :b_id]}))
+          .for_request(Smartsheet::API::RequestSpec.new(path_args: {a_id: 123, b_id: 234}, params:{}, header_overrides: {}, body:{}))
+      @url_builder.build().must_equal 'https://api.smartsheet.com/2.0/a/123/b/234'
+    end
 
-      it 'handles symbols' do
-        @object.build_url(*['a', :a_id, 'b', :b_id], context: {a_id: 123, b_id: 234}).must_equal 'https://api.smartsheet.com/2.0/a/123/b/234'
-      end
-
-      it 'raises on missing symbol' do
-        # TODO: This isn't great behavior. We should probably throw some sort of exception when the context is incomplete
-        @object.build_url(*['a', :a_id, 'b', :b_id], context: {a_id: 123}).must_equal 'https://api.smartsheet.com/2.0/a/123/b/'
-      end
+    it 'raises on missing symbol' do
+      @url_builder
+          .for_endpoint(Smartsheet::API::EndpointSpec.new({symbol: :asdf, method: :get, url: ['a', :a_id, 'b', :b_id]}))
+          .for_request(Smartsheet::API::RequestSpec.new(path_args: {a_id: 123}, params:{}, header_overrides: {}, body:{}))
+      -> {@url_builder.build()}.must_raise RuntimeError
     end
   end
 end
