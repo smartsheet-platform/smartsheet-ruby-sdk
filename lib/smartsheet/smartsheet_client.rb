61493a0f03fc9b929ae9b50374e063e1ef457ae1
Cameron Bowie <cameron.bowie@parivedasolutions.com>
2017-09-22 11:37:07 -0700

    Add server info endpoints

--- lib/smartsheet/smartsheet_client.rb
+++ lib/smartsheet/smartsheet_client.rb
@@ -3,15 +3,17 @@ require_relative 'api/middleware/error_translator'
 require_relative 'api/middleware/response_parser'
 
 require_relative 'endpoints/sheets/sheets'
+require_relative 'endpoints/server_info/server_info'
 
 module Smartsheet
   class SmartsheetClient
-    attr_reader :sheets
+    attr_reader :sheets, :server_info
 
     def initialize(token)
       @net_client = API::NetClient.new(token)
 
       @sheets = Sheets.new(@net_client)
+      @server_info = ServerInfo.new(@net_client)
     end
   end
 end
