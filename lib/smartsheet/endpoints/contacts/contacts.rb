429ada9538c729b30152b31d18895bcc0f7bf8a1
Cameron Bowie <cameron.bowie@parivedasolutions.com>
2017-09-22 11:47:25 -0700

    Add contacts

---
+++ lib/smartsheet/endpoints/contacts/contacts.rb
@@ -0,0 +1,28 @@
+module Smartsheet
+  class Contacts
+    attr_reader :client
+    private :client
+
+    def initialize(client)
+      @client = client
+    end
+
+    def get(contact_id:, header_overrides: {})
+      endpoint_spec = Smartsheet::API::EndpointSpec.new(:get, ['contacts', :contact_id])
+      request_spec = Smartsheet::API::RequestSpec.new(
+          header_overrides: header_overrides,
+          contact_id: contact_id
+      )
+      client.make_request(endpoint_spec, request_spec)
+    end
+
+    def list(params: {}, header_overrides: {})
+      endpoint_spec = Smartsheet::API::EndpointSpec.new(:get, ['contacts'])
+      request_spec = Smartsheet::API::RequestSpec.new(
+          header_overrides: header_overrides,
+          params: params
+      )
+      client.make_request(endpoint_spec, request_spec)
+    end
+  end
+end
--- lib/smartsheet/smartsheet_client.rb
+++ lib/smartsheet/smartsheet_client.rb
@@ -4,16 +4,18 @@ require_relative 'api/middleware/response_parser'
 
 require_relative 'endpoints/sheets/sheets'
 require_relative 'endpoints/server_info/server_info'
+require_relative 'endpoints/contacts/contacts'
 
 module Smartsheet
   class SmartsheetClient
-    attr_reader :sheets, :server_info
+    attr_reader :sheets, :server_info, :contacts
 
     def initialize(token)
       @net_client = API::NetClient.new(token)
 
       @sheets = Sheets.new(@net_client)
       @server_info = ServerInfo.new(@net_client)
+      @contacts = Contacts.new(@net_client)
     end
   end
 end
