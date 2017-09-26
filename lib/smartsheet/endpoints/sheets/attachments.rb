d2b7bdea4e2597084b6402433ecc563dedcceec3
Nathan Armstrong <kenogu.labz@gmail.com>
2017-09-25 17:16:56 -0700

    Add some attachment endpoints

---
+++ lib/smartsheet/endpoints/sheets/attachments.rb
@@ -0,0 +1,50 @@
+module Smartsheet
+  class SheetsAttachments
+    attr_reader :client
+    private :client
+
+    def initialize(client)
+      @client = client
+    end
+
+    def list_attachments(sheet_id:, params: {}, header_overrides: {})
+      endpoint_spec = Smartsheet::API::EndpointSpec.new(:get, ['sheets', :sheet_id, 'attachments'])
+      request_spec = Smartsheet::API::RequestSpec.new(
+          params: params,
+          header_overrides: header_overrides,
+          sheet_id: sheet_id
+      )
+      client.make_request(endpoint_spec, request_spec)
+    end
+
+    def get_attachment(sheet_id:, attachment_id:, header_overrides: {})
+      endpoint_spec = Smartsheet::API::EndpointSpec.new(:get, ['sheets', :sheet_id, 'attachments', :attachment_id])
+      request_spec = Smartsheet::API::RequestSpec.new(
+          header_overrides: header_overrides,
+          sheet_id: sheet_id,
+          attachment_id: attachment_id
+      )
+      client.make_request(endpoint_spec, request_spec)
+    end
+
+    def attach_url(sheet_id:, body:, header_overrides: {})
+      endpoint_spec = Smartsheet::API::EndpointSpec.new(:post, ['sheets', :sheet_id, 'attachments'], body_type: :json)
+      request_spec = Smartsheet::API::RequestSpec.new(
+          header_overrides: header_overrides,
+          body: body,
+          sheet_id: sheet_id
+      )
+      client.make_request(endpoint_spec, request_spec)
+    end
+
+    def delete(sheet_id:, attachment_id:, header_overrides: {})
+      endpoint_spec = Smartsheet::API::EndpointSpec.new(:delete, ['sheets', :sheet_id, 'attachments', :attachment_id])
+      request_spec = Smartsheet::API::RequestSpec.new(
+          header_overrides: header_overrides,
+          sheet_id: sheet_id,
+          attachment_id: attachment_id
+      )
+      client.make_request(endpoint_spec, request_spec)
+    end
+  end
+end
---
+++ test/smartsheet/endpoints/sheets/attachments_test.rb
@@ -0,0 +1,56 @@
+require 'faraday'
+require_relative '../../../test_helper'
+require_relative '../endpoint_test_helper'
+
+class SheetAttachmentTest < Minitest::Test
+  extend Smartsheet::Test::EndpointHelper
+
+  attr_accessor :mock_client
+  attr_accessor :smartsheet_client
+
+  def category
+    smartsheet_client.sheets.attachments
+  end
+
+  def self.endpoints
+    [
+        {
+            symbol: :list_attachments,
+            method: :get,
+            url: ['sheets', :sheet_id, 'attachments'],
+            args: {sheet_id: 123},
+            has_params: true,
+            headers: nil
+        },
+        {
+            symbol: :get_attachment,
+            method: :get,
+            url: ['sheets', :sheet_id, 'attachments', :attachment_id],
+            args: {sheet_id: 123, attachment_id: 123},
+            has_params: false,
+            headers: nil
+        },
+        {
+            symbol: :attach_url,
+            method: :post,
+            url: ['sheets', :sheet_id, 'attachments'],
+            args: {sheet_id: 123, body: {}},
+            has_params: false,
+            headers: nil
+        },
+        {
+            symbol: :delete,
+            method: :delete,
+            url: ['sheets', :sheet_id, 'attachments', :attachment_id],
+            args: {sheet_id: 123, attachment_id: 123},
+            has_params: false,
+            headers: nil
+        },
+    ]
+  end
+
+  define_setup
+  define_endpoints_tests
+end
+
+
