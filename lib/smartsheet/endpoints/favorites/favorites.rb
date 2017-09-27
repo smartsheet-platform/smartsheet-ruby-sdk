8458c4ba5ca2f90339e151f8aef8a280ae1b52b6
Cameron Bowie <cameron.bowie@parivedasolutions.com>
2017-09-27 16:00:01 -0700

    Add favorites

--- lib/smartsheet/endpoints/favorites/favorites.rb
+++ lib/smartsheet/endpoints/favorites/favorites.rb
@@ -24,5 +24,121 @@ module Smartsheet
       )
       client.make_request(endpoint_spec, request_spec)
     end
+
+    def remove_folder(folder_id:, header_overrides: {})
+      remove_favorite(
+          url: ['folder', :folder_id],
+          header_overrides: header_overrides,
+          folder_id: folder_id
+      )
+    end
+
+    def remove_report(report_id:, header_overrides: {})
+      remove_favorite(
+          url: ['report', :report_id],
+          header_overrides: header_overrides,
+          report_id: report_id
+      )
+    end
+
+    def remove_sheet(sheet_id:, header_overrides: {})
+      remove_favorite(
+          url: ['sheet', :sheet_id],
+          header_overrides: header_overrides,
+          sheet_id: sheet_id
+      )
+    end
+
+    def remove_sight(sight_id:, header_overrides: {})
+      remove_favorite(
+          url: ['sights', :sight_id],
+          header_overrides: header_overrides,
+          sight_id: sight_id
+      )
+    end
+
+    def remove_template(template_id:, header_overrides: {})
+      remove_favorite(
+          url: ['template', :template_id],
+          header_overrides: header_overrides,
+          template_id: template_id
+      )
+    end
+
+    def remove_workspace(workspace_id:, header_overrides: {})
+      remove_favorite(
+          url: ['workspace', :workspace_id],
+          header_overrides: header_overrides,
+          workspace_id: workspace_id
+      )
+    end
+
+    def remove_folders(folder_ids:, header_overrides: {})
+      remove_favorites(
+          url: ['folder'],
+          header_overrides: header_overrides,
+          object_ids: folder_ids
+      )
+    end
+
+    def remove_reports(report_ids:, header_overrides: {})
+      remove_favorites(
+          url: ['report'],
+          header_overrides: header_overrides,
+          object_ids: report_ids
+      )
+    end
+
+    def remove_sheets(sheet_ids:, header_overrides: {})
+      remove_favorites(
+          url: ['sheet'],
+          header_overrides: header_overrides,
+          object_ids: sheet_ids
+      )
+    end
+
+    def remove_sights(sight_ids:, header_overrides: {})
+      remove_favorites(
+          url: ['sights'],
+          header_overrides: header_overrides,
+          object_ids: sight_ids
+      )
+    end
+
+    def remove_templates(template_ids:, header_overrides: {})
+      remove_favorites(
+          url: ['template'],
+          header_overrides: header_overrides,
+          object_ids: template_ids
+      )
+    end
+
+    def remove_workspaces(workspace_ids:, header_overrides: {})
+      remove_favorites(
+          url: ['workspace'],
+          header_overrides: header_overrides,
+          object_ids: workspace_ids
+      )
+    end
+
+    private
+
+    def remove_favorite(url:, header_overrides:, **url_args)
+      endpoint_spec = Smartsheet::API::EndpointSpec.new(:delete, ['favorites'] + url)
+      request_spec = Smartsheet::API::RequestSpec.new(
+          header_overrides: header_overrides,
+          **url_args
+      )
+      client.make_request(endpoint_spec, request_spec)
+    end
+
+    def remove_favorites(object_ids:, url:, header_overrides:)
+      endpoint_spec = Smartsheet::API::EndpointSpec.new(:delete, ['favorites'] + url)
+      request_spec = Smartsheet::API::RequestSpec.new(
+          header_overrides: header_overrides,
+          params: {objectIds: object_ids}
+      )
+      client.make_request(endpoint_spec, request_spec)
+    end
   end
 end
