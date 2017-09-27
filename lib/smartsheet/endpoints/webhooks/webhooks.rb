685aeca44b02b6837e062ae67283ddc82b64aeeb
Cameron Bowie <cameron.bowie@parivedasolutions.com>
2017-09-27 11:19:21 -0700

    Add webhooks

---
+++ lib/smartsheet/endpoints/webhooks/webhooks.rb
@@ -0,0 +1,64 @@
+module Smartsheet
+  class Webhooks
+    attr_reader :client
+    private :client
+
+    def initialize(client)
+      @client = client
+    end
+
+    def create(body:, header_overrides: {})
+      endpoint_spec = Smartsheet::API::EndpointSpec.new(:post, ['webhooks'], body_type: :json)
+      request_spec = Smartsheet::API::RequestSpec.new(
+          header_overrides: header_overrides,
+          body: body
+      )
+      client.make_request(endpoint_spec, request_spec)
+    end
+
+    def delete(webhook_id:, header_overrides: {})
+      endpoint_spec = Smartsheet::API::EndpointSpec.new(:delete, ['webhooks', :webhook_id])
+      request_spec = Smartsheet::API::RequestSpec.new(
+          header_overrides: header_overrides,
+          webhook_id: webhook_id
+      )
+      client.make_request(endpoint_spec, request_spec)
+    end
+
+    def get(webhook_id:, header_overrides: {})
+      endpoint_spec = Smartsheet::API::EndpointSpec.new(:get, ['webhooks', :webhook_id])
+      request_spec = Smartsheet::API::RequestSpec.new(
+          header_overrides: header_overrides,
+          webhook_id: webhook_id
+      )
+      client.make_request(endpoint_spec, request_spec)
+    end
+
+    def list(header_overrides: {})
+      endpoint_spec = Smartsheet::API::EndpointSpec.new(:get, ['webhooks'])
+      request_spec = Smartsheet::API::RequestSpec.new(
+          header_overrides: header_overrides
+      )
+      client.make_request(endpoint_spec, request_spec)
+    end
+
+    def reset_shared_secret(webhook_id:, header_overrides: {})
+      endpoint_spec = Smartsheet::API::EndpointSpec.new(:post, ['webhooks', :webhook_id, 'resetsharedsecret'])
+      request_spec = Smartsheet::API::RequestSpec.new(
+          header_overrides: header_overrides,
+          webhook_id: webhook_id
+      )
+      client.make_request(endpoint_spec, request_spec)
+    end
+
+    def update(webhook_id:, body:, header_overrides: {})
+      endpoint_spec = Smartsheet::API::EndpointSpec.new(:put, ['webhooks', :webhook_id], body_type: :json)
+      request_spec = Smartsheet::API::RequestSpec.new(
+          header_overrides: header_overrides,
+          body: body,
+          webhook_id: webhook_id
+      )
+      client.make_request(endpoint_spec, request_spec)
+    end
+  end
+end
--- lib/smartsheet/smartsheet_client.rb
+++ lib/smartsheet/smartsheet_client.rb
@@ -2,42 +2,43 @@ require 'smartsheet/api/net_client'
 require 'smartsheet/api/middleware/error_translator'
 require 'smartsheet/api/middleware/response_parser'
 
-require 'smartsheet/endpoints/sheets/sheets'
-require 'smartsheet/endpoints/server_info/server_info'
 require 'smartsheet/endpoints/contacts/contacts'
-require 'smartsheet/endpoints/search/search'
 require 'smartsheet/endpoints/folders/folders'
 require 'smartsheet/endpoints/groups/groups'
 require 'smartsheet/endpoints/home/home'
 require 'smartsheet/endpoints/reports/reports'
+require 'smartsheet/endpoints/search/search'
+require 'smartsheet/endpoints/server_info/server_info'
+require 'smartsheet/endpoints/sheets/sheets'
 require 'smartsheet/endpoints/sights/sights'
 require 'smartsheet/endpoints/templates/templates'
 require 'smartsheet/endpoints/update_requests/update_requests'
 require 'smartsheet/endpoints/users/users'
+require 'smartsheet/endpoints/webhooks/webhooks'
 require 'smartsheet/endpoints/workspaces/workspaces'
 
 
 module Smartsheet
   class SmartsheetClient
-    attr_reader :sheets, :server_info, :contacts, :search, :folders
-    attr_reader :groups, :home, :reports, :sights, :templates, :update_requests
-    attr_reader :users, :workspaces
+    attr_reader :contacts, :folders, :groups, :home, :reports, :search, :server_info, :sheets, :sights, :templates
+    attr_reader :update_requests, :users, :webhooks, :workspaces
 
     def initialize(token)
       @net_client = API::NetClient.new(token)
 
-      @sheets = Sheets.new(@net_client)
-      @server_info = ServerInfo.new(@net_client)
       @contacts = Contacts.new(@net_client)
-      @search = Search.new(@net_client)
       @folders = Folders.new(@net_client)
       @groups = Groups.new(@net_client)
       @home = Home.new(@net_client)
       @reports = Reports.new(@net_client)
+      @search = Search.new(@net_client)
+      @server_info = ServerInfo.new(@net_client)
+      @sheets = Sheets.new(@net_client)
       @sights = Sights.new(@net_client)
       @templates = Templates.new(@net_client)
       @update_requests = UpdateRequests.new(@net_client)
       @users = Users.new(@net_client)
+      @webhooks = Webhooks.new(@net_client)
       @workspaces = Workspaces.new(@net_client)
     end
   end
