542c3b312733b94d7e691092d0979357e37cc04c
Nathan Armstrong <kenogu.labz@gmail.com>
2017-10-03 11:31:49 -0700

    Update responses to carry more metadata.

--- lib/smartsheet/api/faraday_adapter/faraday_response.rb
+++ lib/smartsheet/api/faraday_adapter/faraday_response.rb
@@ -1,11 +1,11 @@
 module Smartsheet
   module API
     class FaradayResponse
-      def self.from_result(result)
-        if result.respond_to?(:errorCode)
-          FaradayErrorResponse.new(result)
+      def self.from_result(faraday_response)
+        if faraday_response[:body].respond_to?(:errorCode)
+          FaradayErrorResponse.new(faraday_response[:body], faraday_response)
         else
-          FaradaySuccessResponse.new(result)
+          FaradaySuccessResponse.new(faraday_response[:body], faraday_response)
         end
       end
     end
@@ -13,12 +13,15 @@ module Smartsheet
     class FaradayErrorResponse
       RETRYABLE_ERRORS = 4001..4004
 
-      attr_reader :error_code, :message, :ref_id
+      attr_reader :error_code, :message, :ref_id, :status_code, :reason_phrase, :headers
 
-      def initialize(result)
+      def initialize(result, faraday_response)
         @error_code = result.errorCode
         @message = result.message
         @ref_id = result.refId
+        @status_code = faraday_response[:status]
+        @reason_phrase = faraday_response[:reason_phrase]
+        @headers = faraday_response[:response_headers]
       end
 
       def should_retry?
@@ -31,10 +34,13 @@ module Smartsheet
     end
 
     class FaradaySuccessResponse
-      attr_reader :result
+      attr_reader :result, :status_code, :reason_phrase, :headers
 
-      def initialize(result)
+      def initialize(result, faraday_response)
         @result = result
+        @status_code = faraday_response[:status]
+        @reason_phrase = faraday_response[:reason_phrase]
+        @headers = faraday_response[:response_headers]
       end
 
       def should_retry?
