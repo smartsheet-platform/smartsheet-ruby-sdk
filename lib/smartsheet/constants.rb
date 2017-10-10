0bfa4b4328881461aa084c0ede7196fd7cd4431a
Nathan Armstrong <kenogu.labz@gmail.com>
2017-10-10 16:30:19 -0700

    Add required Ruby version to the gemspec.

--- lib/smartsheet/constants.rb
+++ lib/smartsheet/constants.rb
@@ -1,6 +1,6 @@
 module Smartsheet
   module Constants
-    VERSION = '1.0.0.beta.0'.freeze
+    VERSION = '1.0.0.beta.1'.freeze
 
     USER_AGENT = 'smartsheet-ruby-sdk'.freeze
     API_URL = 'https://api.smartsheet.com/2.0'.freeze
