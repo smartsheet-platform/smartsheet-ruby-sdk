7b6801653859df87650266676651045ae790c8d3
Cameron Bowie <cameron.bowie@parivedasolutions.com>
2017-10-10 10:16:34 -0700

    Move retry configs to constants file

--- lib/smartsheet/constants.rb
+++ lib/smartsheet/constants.rb
@@ -9,5 +9,8 @@ module Smartsheet
     EXCEL_TYPE = 'application/vnd.ms-excel'.freeze
     PDF_TYPE = 'application/pdf'.freeze
     CSV_TYPE = 'text/csv'.freeze
+
+    DEFAULT_MAX_RETRY_TIME = 15
+    DEFAULT_BACKOFF_METHOD = proc { |iteration, _result| 2**iteration + rand }
   end
 end
