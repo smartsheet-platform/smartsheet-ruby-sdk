a5d65ffe9ab5821643dc867a204af8fb02274203
Nathan Armstrong <kenogu.labz@gmail.com>
2017-10-11 13:28:37 -0700

    Ensure expected logs are output from the retry net client decorator.

--- test/smartsheet/api/retry_net_client_decorator_test.rb
+++ test/smartsheet/api/retry_net_client_decorator_test.rb
@@ -6,6 +6,13 @@ require 'timecop'
 describe Smartsheet::API::RetryNetClientDecorator do
   include Smartsheet::Test
 
+  REQUEST = Smartsheet::API::Request.new(
+      'token',
+      Smartsheet::API::EndpointSpec.new(:get, ['sheets']),
+      Smartsheet::API::RequestSpec.new,
+      @base_url
+  )
+
   before do
     @response = mock
     @response.stubs(:'success?').returns false
@@ -40,36 +47,103 @@ describe Smartsheet::API::RetryNetClientDecorator do
 
   it 'does not retry when a non-retryable failure occurs' do
     given_non_retryable_response
-    @client.expects(:make_request).returns @response
+    @client.expects(:make_request).with(REQUEST).returns @response
 
     Smartsheet::API::RetryNetClientDecorator
       .new(@client, Smartsheet::API::RetryLogic.new)
-      .make_request(
-        Smartsheet::API::Request.new(
-          'token',
-          Smartsheet::API::EndpointSpec.new(:get, ['sheets']),
-          Smartsheet::API::RequestSpec.new,
-          @base_url
-        )
-      )
+      .make_request(REQUEST)
   end
 
   it 'retries when a retryable failure occurs' do
     given_retryable_response
-    @client.expects(:make_request).at_least(2).returns @response
+    @client.expects(:make_request).with(REQUEST).at_least(2).returns @response
 
     retrier = Smartsheet::API::RetryLogic.new
     stub_sleep(retrier)
 
     Smartsheet::API::RetryNetClientDecorator
       .new(@client, retrier)
-      .make_request(
-        Smartsheet::API::Request.new(
-          'token',
-          Smartsheet::API::EndpointSpec.new(:get, ['sheets']),
-          Smartsheet::API::RequestSpec.new,
-          @base_url
-        )
-      )
+      .make_request(REQUEST)
+  end
+
+  it 'logs retry failure count after retrying more than once' do
+    given_retryable_response
+    @client.stubs(:make_request).with(REQUEST).returns @response
+
+    retrier = Smartsheet::API::RetryLogic.new
+    stub_sleep(retrier)
+
+    logger = mock
+    logger.expects(:log_retry_failure).with { |x| x >= 2 }
+    logger.stubs(:log_retry_attempt)
+
+    Smartsheet::API::RetryNetClientDecorator
+        .new(@client, retrier, logger: logger)
+        .make_request(REQUEST)
+  end
+
+  it 'logs retry attempts when a retryable failure occurs' do
+    given_retryable_response
+    @client.stubs(:make_request).with(REQUEST).returns @response
+
+    retrier = Smartsheet::API::RetryLogic.new
+    stub_sleep(retrier)
+
+    logger = mock
+    logger.stubs(:log_retry_failure)
+
+    curr_attempt = 0
+    logger.expects(:log_retry_attempt)
+        .at_least_once
+        .with do |log_request, log_response, attempt_num|
+          curr_attempt = curr_attempt + 1
+          log_request == REQUEST && log_response == @response && attempt_num == curr_attempt
+        end
+
+    Smartsheet::API::RetryNetClientDecorator
+        .new(@client, retrier, logger: logger)
+        .make_request(REQUEST)
+  end
+
+  it 'does not log retry attempts when a non-retryable failure occurs' do
+    given_non_retryable_response
+    @client.stubs(:make_request).with(REQUEST).returns @response
+
+    logger = mock
+    logger.stubs(:log_retry_failure)
+    logger.expects(:log_retry_attempt).never
+
+    Smartsheet::API::RetryNetClientDecorator
+        .new(@client, Smartsheet::API::RetryLogic.new)
+        .make_request(REQUEST)
+  end
+
+  it 'logs retry failure count after retrying more than once' do
+    given_retryable_response
+    @client.stubs(:make_request).with(REQUEST).returns @response
+
+    retrier = Smartsheet::API::RetryLogic.new
+    stub_sleep(retrier)
+
+    logger = mock
+    logger.expects(:log_retry_failure).with { |x| x >= 2 }
+    logger.stubs(:log_retry_attempt)
+
+    Smartsheet::API::RetryNetClientDecorator
+        .new(@client, retrier, logger: logger)
+        .make_request(REQUEST)
+  end
+
+  it 'does not log retry failure count after retrying only once' do
+    given_non_retryable_response
+    @client.stubs(:make_request).with(REQUEST).returns @response
+
+    logger = mock
+    logger.expects(:log_retry_failure).never
+    logger.stubs(:log_retry_attempt)
+
+    Smartsheet::API::RetryNetClientDecorator
+        .new(@client, Smartsheet::API::RetryLogic.new, logger: logger)
+        .make_request(REQUEST)
   end
 end
