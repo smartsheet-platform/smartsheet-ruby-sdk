8167b74bc837c78dcf7af73b1d6c44f947c57c16
Nathan Armstrong <kenogu.labz@gmail.com>
2018-02-09 13:25:05 -0800

    Add quick docs to the endpoint and request spec initializers.

--- lib/smartsheet/api/endpoint_spec.rb
+++ lib/smartsheet/api/endpoint_spec.rb
@@ -6,6 +6,24 @@ module Smartsheet
     class EndpointSpec
       attr_reader :method, :url_segments, :spec
 
+      # @param method [Symbol] The HTTP method for the endpoint; one of
+      #   `[:get, :put, :post, :delete]`
+      #
+      # @param url_segments [Array<String, Symbol>] The segments of the endpoint URL; strings are
+      #   added as literal segments, while symbols are mapped to corresponding values in a request
+      #   specification
+      # 
+      # @param spec [Hash{Symbol=>Object}] Optional params, the following of which are supported:
+      #
+      #    - `:no_auth` - If specified as a key, the endpoint can be called without authentication.
+      #        To preserve meaning, it is recommended to associate `:no_auth` with the value `true`.
+      #
+      #    - `:body_type` - If specified as a key, the endpoint will require a body to be provided
+      #        by the request.  When associated with `:json`, it will expect a JSON formattable
+      #        body.  When associated with `:file`, it will expect file upload parameters.
+      #
+      #    - `:headers` - When specified, this is expected to be a map of static HTTP headers that
+      #        will be attached to each request.
       def initialize(method, url, **spec)
         @method = method
         @url_segments = url
